7cd671531b16433a40bbcd3c305997bc374e79f5
Abdullah <boomjay008@gmail.com>
2024-01-08 08:16:13 +0100

    feat: setup the redux-store

---
+++ RTK/app/store.ts
@@ -0,0 +1,34 @@
+import cartReducer from "@/RTK/features/cart-slice";
+import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const logger = createLogger();
+// Define your reducers
+const rootReducer = combineReducers({
+  cartReducer: cartReducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create the store
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: false,
+  }).concat(logger),
+  devTools: process.env.NODE_ENV === 'development' ? true : false,
+});
+
+export const persistedStore = persistStore(store);
+
+export type StoreType = ReturnType<typeof configureStore>;
+export type RootState = ReturnType<typeof store.getState>;
+export type Dispatch = typeof store.dispatch;
---
+++ RTK/features/cart-slice/index.tsx
@@ -0,0 +1,51 @@
+import { Cart, CartItem } from '@/lib/types';
+import { createSlice } from '@reduxjs/toolkit';
+
+type CartState = {
+  cart: Cart;
+  cartItem: CartItem | null;
+}
+
+const cartInitialState: CartItem = {
+  id: 0,
+  name: '',
+  price: 0,
+  quantity: 0,
+  size: '',
+};
+const CartInitialState: Cart = [];
+
+const initialCartState : CartState = {
+  cart: CartInitialState,
+  cartItem: null,
+};
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialCartState,
+  reducers: {
+    addCartItem: (state, action) => {
+      const addedItem = {...state.cartItem, ...action.payload};
+      state.cartItem = addedItem;
+      state.cart = [...state.cart, addedItem];
+    },
+    removeCartItem: (state, action) => {
+      const { id } = action.payload;
+      state.cart = state.cart.filter(item => item.id !== id);
+    },
+    updateQuantity: (state, action) => {
+      const { id, quantity } = action.payload;
+      const item = state.cart.find(item => item.id === id);
+      if (item) {
+        item.quantity = quantity;
+      }
+      state.cart = [...state.cart];
+    },
+    getAllCartItems: (state) => {
+      state.cart;
+    },
+  },
+});
+
+export const { addCartItem, removeCartItem, updateQuantity } = cartSlice.actions;
+export default cartSlice.reducer;
